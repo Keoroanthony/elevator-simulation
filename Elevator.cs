fc45be807548f955c1c2675bfa92213b193c86b8
anthonymaoga <anthony.maoga@jumia.com>
2023-06-24 21:03:51 +0300

    dynamic inputs

--- Elevator.cs
+++ Elevator.cs
@@ -129,19 +129,42 @@ namespace ElevatorSimulation
 
     class Program
     {
-        static void Main(string[] args)
+        
+     static void Main(string[] args)
         {
             // Create an elevator system with 3 elevators and a maximum occupancy of 10
             ElevatorSystem elevatorSystem = new ElevatorSystem(numElevators: 3, maxOccupancy: 10);
-
-            // Call elevator to floor 5
-            elevatorSystem.CallElevator(5);
-
-            // Set 3 occupants on floor 3
-            elevatorSystem.SetOccupants(3, 3);
-
-            // Show the status of elevators
-            elevatorSystem.ShowStatus();
+        
+            while (true)
+            {
+                Console.WriteLine("Enter the floor you are currently on:");
+                int currentFloor = int.Parse(Console.ReadLine());
+        
+                Console.WriteLine("Enter the floor you want to go to:");
+                int targetFloor = int.Parse(Console.ReadLine());
+        
+                // Call elevator to the current floor
+                elevatorSystem.CallElevator(currentFloor);
+        
+                // Set occupants on the current floor
+                Console.WriteLine("Enter the number of occupants on the current floor:");
+                int occupants = int.Parse(Console.ReadLine());
+                elevatorSystem.SetOccupants(currentFloor, occupants);
+        
+                // Move the elevator to the target floor
+                elevatorSystem.CallElevator(targetFloor);
+        
+                // Show the status of elevators
+                elevatorSystem.ShowStatus();
+        
+                Console.WriteLine("Press any key to continue or 'q' to quit...");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "q")
+                    break;
+        
+                Console.Clear();
+            }
         }
+
     }
 }
